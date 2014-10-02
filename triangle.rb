db207ac378047c27405959c350e9e56dddc4b755
Igor Serpak <igor.serpak@gmail.com>
2014-10-02 19:39:07 +0300

    work done at work

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,22 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  raise TriangleError, "Length of all sides in a triangle should be greater than 0" if a <= 0 || b <= 0 || c <= 0
+  x, y, z = [a, b, c].sort
+  raise TriangleError, "Length of the longest side should be smaller than the length of the other two" if x + y <= z
+  if a == b
+    if b == c
+      :equilateral
+    else
+      :isosceles
+    end
+  elsif b == c
+    :isosceles
+  elsif a == c
+    :isosceles
+  else
+    :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
